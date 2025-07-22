21d4a150c75c952bef1f75e34c45a7c4dbd75ac6
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-22 18:20:31 -0400

    routes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export class DashboardComponent {}
+export default class DashboardComponent {}
