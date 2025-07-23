f7f43f48fdaec3087d337da47a9a938280f51818
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-23 17:05:19 -0400

    api service

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { UsersService } from '@app/services/users.service';
+import { TitleComponent } from '@app/shared/title/title.component';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [TitleComponent, RouterLink],
   templateUrl: './users.component.html',
-  styles: ``,
 })
-export default class UsersComponent {}
+export default class UsersComponent {
+  public usersService = inject(UsersService);
+}
